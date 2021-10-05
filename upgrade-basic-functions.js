d2ccb7abd0eae404b4eaeede18f57a832242f91a
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-05 18:04:54 +0200

    iteration#1

--- upgrade-basic-functions.js
+++ upgrade-basic-functions.js
@@ -1,3 +1,14 @@
 //////// ITERACION 1 ////////
 
 //------------------- 1.1
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log(numberOne)
+    } else {
+        console.log(numberTwo)
+  }
+}
+
+//------------------- 1.2
+
