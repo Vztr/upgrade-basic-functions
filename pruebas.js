fa4c2c84e14061ec025c3dee31c6168f79a632bd
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-06 12:40:49 +0200

    done

--- pruebas.js
+++ pruebas.js
@@ -1,82 +1,4 @@
-
-const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
-  let longestWord = [0];
-
-  for (let i = 0; i < array.length; i++) {
-      if(array[i].length > longestWord.length) {
-          longestWord = array[i];
-      } 
-    }    
-  console.log(findLongestWord);
-}
-
-findLongestWord(avengers);
-
-/* Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
-Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.  */
-
-const numbers = [1, 2, 3, 5, 45, 37, 58];
-
-function sumAll(param) {
-  // insert code
-}
-
-/* Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:*/
-
-const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average(param) {
-  // insert code
-}
-
-/* Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. */
-
-const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
-function averageWord(param) {
-  // insert code
-}
-
-/* Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. */
-
-const duplicates = [
-    'sushi',
-    'pizza',
-    'burger',
-    'potatoe',
-    'pasta',
-    'ice-cream',
-    'pizza',
-    'chicken',
-    'onion rings',
-    'pasta',
-    'soda'
-  ];
-  function removeDuplicates(param) {
-    // insert code
-  }
-
-  /* Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. */
-
-  const nameFinder = [
-    'Peter',
-    'Steve',
-    'Tony',
-    'Natasha',
-    'Clint',
-    'Logan',
-    'Xabier',
-    'Bruce',
-    'Peggy',
-    'Jessica',
-    'Marc'
-  ];
-  function finderName(param) {
-    // insert code
-  }
-
-  /* Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma. */
-
-  const counterWords = [
+ const counterWords = [
     'code',
     'repeat',
     'eat',
@@ -90,5 +12,13 @@ const duplicates = [
     'code'
   ];
   function repeatCounter(param) {
-    // insert code
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+      if(array[i] in count) {
+        count[array[i]]++
+      } else {
+        count[array[i]] = 1
+      }
+    }
+    return count
   }
--- upgrade-basic-functions.js
+++ upgrade-basic-functions.js
@@ -2,13 +2,149 @@
 
 //------------------- 1.1
 
+let result;
+
 function sum(numberOne , numberTwo) {
     if (numberOne > numberTwo) {
-        console.log(numberOne)
+        result = numberOne
     } else {
-        console.log(numberTwo)
+        result = numberTwo
   }
 }
 
 //------------------- 1.2
 
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+  let longestWord = param[0];
+
+  for (let i = 0; i < longestWord.length; i++) {
+      if(param[i] && (param[i].length > longestWord.length)) {
+          longestWord = param[i];
+      } 
+    }    
+  console.log(findLongestWord);
+}
+
+findLongestWord(avengers);
+
+//------------------- 1.3
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) { // Recorremos el array
+    result += array[i] // añadimos los valores que cogemos del array a "result", y a la vez sumamos los elementos guardados en cada iteración del bucle
+  }
+  return result // Devolvemos el resultado cuando se haya recorrido todo el bucle y esten almacenadas en "result" todos los elementos sumados.
+}
+
+console.log(sumAll(numbers)); // Invocamos la función pasandole un parámetro.
+
+//------------------- 1.4
+
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+
+function average(array) {
+  let average = sumAll(array) / array.length; // Invocamos una función dentro de otra función
+  return average
+}
+
+//------------------- 1.5
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if(typeof array[i] === "string") {
+      count += array[i].legth
+    } else {
+      count += array[i]
+    }
+  }
+  return count
+}
+
+console.log(averageWord(mixedElements));
+
+//------------------- 1.6
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potato',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+  function removeDuplicates(array) {
+    let newElements = [];
+    for (let i = 0; i < array.length; i++) {
+      if(!newElements.includes(array[i])) {
+        newElements.push(array[i])
+      }
+    }
+    return newElements;
+  }
+
+  console.log(removeDuplicates(duplicates));
+
+  //------------------- 1.7
+
+  const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  function finderName(array) {
+    let result;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === name) {
+        return i
+      }   
+    }
+    return "No has resultado";
+  }
+
+  console.log(finderName(nameFinder, "Logan"));
+
+    //------------------- 1.8
+
+    const counterWords = [
+        'code',
+        'repeat',
+        'eat',
+        'sleep',
+        'code',
+        'enjoy',
+        'sleep',
+        'code',
+        'enjoy',
+        'upgrade',
+        'code'
+      ];
+      function repeatCounter(param) {
+        let count = 0;
+        for (let i = 0; i < array.length; i++) {
+          if(array[i] in count) {
+            count[array[i]]++
+          } else {
+            count[array[i]] = 1
+          }
+        }
+        return count
+      }
